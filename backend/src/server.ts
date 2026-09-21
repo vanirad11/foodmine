17c109ac4f9a9fb7ddf1be6b25184f1f371ddb74
Vani R <vanirad11@gmail.com>
2026-09-21 10:35:46 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200", "https://foodmine-o11b.vercel.app"]
+    origin:["http://localhost:4200"]
 }));
 
 app.use("/api/foods", foodRouter);
